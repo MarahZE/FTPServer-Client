1f25bed5b92aef21220559a1e7453dd1ce27b791
maze6343 <maze6343@student.su.se>
2023-07-29 00:01:04 +0200

    add some changes to server

--- src/Server.java
+++ src/Server.java
@@ -1,16 +1,17 @@
-import javax.imageio.IIOException;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
-
-    static ArrayList<File> files = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
@@ -62,16 +63,124 @@ public class Server {
                         show.setFont(new Font("Arial",Font.BOLD,20));
 
                         filePanel.add(labelFileName);
-                        filePanel.add(download);
                         filePanel.add(show);
+                        filePanel.add(download);
                         panel.add(filePanel);
                         frame.validate();
                         System.out.println(nameOfFile);
+
+                        show.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                JFrame newFrame = new JFrame("Show Content File:" + nameOfFile);
+                                newFrame.setSize(600,600);
+
+                                JPanel contentPanel = new JPanel();
+                                contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+                                JLabel fileNameLabel = new JLabel("");
+                                fileNameLabel.setFont(new Font("Arial",Font.BOLD,18));
+                                fileNameLabel.setBorder(new EmptyBorder(20,0,20,0));
+
+                                int i = nameOfFile.lastIndexOf(".");
+
+                                String fileExtension = nameOfFile.substring(i);
+
+                                if(fileExtension.equalsIgnoreCase(".txt")) {
+                                    fileNameLabel.setText(new String(contentSize));
+                                } else if (fileExtension.equalsIgnoreCase(".png") || fileExtension.equalsIgnoreCase(".jpg")){
+                                    fileNameLabel.setIcon(new ImageIcon(contentSize));
+                                } else if (fileExtension.equalsIgnoreCase(".pdf")) {
+
+                                }
+
+                                newFrame.add(fileNameLabel);
+
+
+                                newFrame.setVisible(true);
+                            }
+                        });
+
+                        download.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                JFrame newFrame = new JFrame("Download");
+                                newFrame.setSize(800,200);
+
+                                JPanel contentPanel = new JPanel();
+                                contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+                                JPanel newPanel = new JPanel();
+                                contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
+
+                                JLabel jLabel = new JLabel("Are you sure to download " + nameOfFile);
+                                jLabel.setFont(new Font("Arial",Font.BOLD,18));
+                                jLabel.setBorder(new EmptyBorder(20,0,20,0));
+                                //jLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                newPanel.add(jLabel);
+
+
+                                JButton yes = new JButton("Yes");
+                                yes.setPreferredSize(new Dimension(50,75));
+                                yes.setFont(new Font("Arial",Font.BOLD,20));
+                                newPanel.add(yes);
+
+                                JButton No = new JButton("No");
+                                No.setPreferredSize(new Dimension(50,75));
+                                No.setFont(new Font("Arial",Font.BOLD,20));
+                                newPanel.add(No);
+
+                                //contentPanel.add(newPanel);
+
+                                newFrame.add(newPanel);
+                                newFrame.setVisible(true);
+
+                                No.addActionListener(new ActionListener() {
+                                    @Override
+                                    public void actionPerformed(ActionEvent e) {
+                                        newFrame.dispose();
+                                    }
+                                });
+
+                                yes.addActionListener(new ActionListener() {
+                                    @Override
+                                    public void actionPerformed(ActionEvent e) {
+                                        File downloadFile = new File(nameOfFile);
+                                        try {
+                                            FileOutputStream fileOutput = new FileOutputStream(downloadFile);
+                                            fileOutput.write(contentSize);
+                                            fileOutput.close();
+
+                                            newFrame.dispose();
+                                        } catch (IOException error) {
+                                            error.printStackTrace();
+                                        }
+
+                                    }
+                                });
+
+                            }
+                        });
+
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+    }
+
+    public static String showContent(byte[] content, String nameFile) {
+        int i = nameFile.lastIndexOf(".");
+
+        String fileExtension = nameFile.substring(i);
+
+        if(fileExtension.equalsIgnoreCase(".txt")) {
+            return new String(content);
+        } else {
+
+            return "";
         }
 
     }
